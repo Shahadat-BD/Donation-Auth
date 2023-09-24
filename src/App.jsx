2bfc47a6fb1bb73e489dd84b6b283f21d5a208d6
Shahadat-BD <shahadatstudent34@gmail.com>
2023-09-24 20:45:42 +0000

    project setup complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className='text-center text-4xl text-red-500 my-10'>This is donation campaign website</h1>
+    </>
+  )
+}
+
+export default App
